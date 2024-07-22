2039c91ed66e271103d31fb1d71bc0215e5bb992
James Ross <154541930+0xJam3z@users.noreply.github.com>
2024-07-22 10:47:40 -0400

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -3,5 +3,5 @@ sudo masscan -p80,443 -iL list --rate=10000 --exclude 255.255.255.255 --wait 0 -
 awk '{ if ($3 == 80) print $4 >> "open_ips80.txt"; else if ($3 == 443) print $4 >> "open_ips443.txt" }' masscan_results.txt &&
 zgrab2 http --port 80 --input-file open_ips80.txt --max-redirects 0 --output-file zgrab_results_80.json &&
 zgrab2 http --port 443 --input-file open_ips443.txt --max-redirects 0 --output-file zgrab_results_443.json &&
-python3 new.py > opendomains
+python3 zgrabparser.py > opendomains
 echo "Success"
